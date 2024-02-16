37710cb25d812a01ca57f731e61a5d9c287eb2e4
Patrick Ineichen <github@frokonet.ch>
2024-02-16 16:36:30 +0100

    feat: 🎸 Add Spotify logo

---
+++ src/components/spotify-logo.tsx
@@ -0,0 +1,15 @@
+
+import { useStore } from '@nanostores/preact';
+import { isHidden } from '@pgl/store';
+
+const SpotifyLogo = () => {
+  const $isHidden = useStore(isHidden);
+
+  return $isHidden ? null : (
+    <a href="https://open.spotify.com/artist/448XePsDHCCARSgf0XhtUQ" className="mr-4 duration-200 md:hover:scale-125 lg:mr-8">
+      <img src="/spotify-logo.png" alt="spotify" className="h-5 w-5 md:h-6 md:w-6 object-cover" />
+    </a>
+  );
+};
+
+export default SpotifyLogo;
