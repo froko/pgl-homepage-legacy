d7419e0767d4819638f7e4e5b06cdcaf046fdd86
Patrick Ineichen <github@frokonet.ch>
2024-02-19 17:53:59 +0100

    feat: 🎸 Update Agenda

--- data/events.ts
+++ data/events.ts
@@ -1,9 +1,9 @@
 import type { Event } from '.';
 
 export const events: Event[] = [
-  { title: '1. Matinée', description: '27.01.2024, 09:45 @ Stadtkeller Luzern' },
-  { title: 'Offene Probe', description: '31.01.2024, 19:45' },
-  { title: '2. Matinée', description: '03.02.2024, 09:45 @ Stadtkeller Luzern' },
-  { title: 'Schmutziger Donnerstag', description: '08.02.2024 ab 05:00 @ Luzern' },
-  { title: 'Chöbu-Samschtig', description: '10.02.2024 ab 18:00 @ Stadtkeller Luzern' }
+  { title: '1. Matinée', description: '15.02.2025, 09:45 @ Stadtkeller Luzern' },
+  { title: 'Offene Probe', description: '19.02.2025, 19:45' },
+  { title: '2. Matinée', description: '22.02.2025, 09:45 @ Stadtkeller Luzern' },
+  { title: 'Schmutziger Donnerstag', description: '27.02.2025 ab 05:00 @ Luzern' },
+  { title: 'Chöbu-Samschtig', description: '01.03.2025 ab 18:00 @ Stadtkeller Luzern' }
 ];
