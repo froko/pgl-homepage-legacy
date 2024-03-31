2cb77839329e6e315976c538a433276a99258278
Patrick Ineichen <github@frokonet.ch>
2024-03-31 21:14:04 +0200

    feat: Vorstandwechsel

--- data/vorstand.ts
+++ data/vorstand.ts
@@ -1,10 +1,10 @@
 import type { Vorstand } from '.';
 
 export const vorstand: Vorstand[] = [
-  { name: 'Beat Thalmann', funktion: 'Präsident' },
-  { name: 'Andi Felber', funktion: 'Tambourmajor und Vize-Präsident' },
+  { name: 'Andi Felber', funktion: 'Präsident' },
+  { name: 'Marco Bucher', funktion: 'Vize-Präsident' },
   { name: 'Markus Helfenstein', funktion: 'Kassier' },
   { name: 'Manuel Brun', funktion: '1. Beisitzer' },
-  { name: 'Gaudenz Ineichen', funktion: '2. Beisitzer' },
-  { name: 'René Sidler', funktion: 'Aktuar' }
+  { name: 'Gaudenz Ineichen', funktion: 'Tambourmajor und 2. Beisitzer' },
+  { name: 'Reto Bühlmann', funktion: 'Aktuar' }
 ];
